88747a9450c954f66d3617b62e755c6ba0efdf21
Jorge Raul Arroyo Perez <SrRapero720@hotmail.com>
2020-11-29 00:59:58 -0600

    cambios en las estrucctura de la clases

--- README.md
+++ README.md
@@ -1,49 +1,58 @@
-# SR-CONSOLE | Consola moderna
-Una consola con funciones bastante utiles para el desarrollo y debug de una aplicacion
+# Sr-Console | Una vista mas organizada
+Una libreria que le da mas color a los logs de tu aplicacion para que sea facilmente comprensible con herramientas utiles.
 
-## INICIALIZAR
-Sobreescribimos la variable por defecto **console** para que sea mas facil el uso de las funciones
+## COMENCEMOS
+En este ejemplo remplazaremos la variable global `console` para que de esta forma de nos sea mas facil acceder a ella
 ```js
-const settings = {
+const SrConsole = require('sr-console');
+const console = new SrConsole({
     _dirname: _dirname
     filter: [
-        "terrible"
+        "none ",
+        "terrible ",
+        "abominable "
     ]
-}
+});
+```
 
+## FUNCIONES BASICAS
+La libreria cuenta con casi todas las funciones que existen en la clase `Console` original.
+Aqui un ejemplo de todas las funciones con los colores que muestran y los valores que returnan
+```js
+console.send('Hola enorme terrible y abominable Mundo!'); // [00:28:46] Hola enorme y Mundo! - Azul
+console.log('Hola Mundo'); // [00:28:46] Hola Mundo - Azul
+console.warn('Hola Peligroso Mundo'); // [00:28:46] Hola Peligroso Mundo - Amarillo
+console.debug('Hola problematico mundo'); // [00:28:46] Hola problematico mundo - Celeste
+console.err('Hola Erroneo Mundo'); // [00:28:46] Hola Erroneo Mundo - Rojo
+console.error('Hola alias de Erroneo Mundo'); // [00:28:46] Hola alias de Erroneo Mundo - Rojo
+console.success('Hola exitoso mundo'); // [00:28:46] Hola exitoso mundo - Verde
+console.trace('Hola guiado mundo'); // Trace: [00:28:46] Hola guiado mundo at... - Fondo rojo / Amarillo
+console.fatalError(new Error('Errores graves').stack); // [00:28:46] Error: Errores graves - Fondo rojo / Amarillo
+console.group("Hola Agrupado Mundo!") // [00:28:46] Hola Agrupado mundo - Magenta
+```
+
+## UTILIDADES
+La libreria no solo cambia de color los logs y les agrega hora, si no tambien cuenta con utilidades como el "Almacenamiento de Logs" y la emision de estos usando Websockets
+
+
+# Ejemplo de Websocket
+Instaciamos nuestra consola y creamos nuestro servidor websocket, despues usamos la funcion `createSocket()` y le agregamos como parametro el servidor websocket
+```js
 const SrConsole = require('sr-console');
-const console = new SrConsole.load(settings);
-```
-
-## FUNCIONES DE LOG
-El SrConsole puede filtrar en la consola informacion que suele ser en proyectos pequeños muy innecesaria
-```js
-//Simple LOG con color y fecha
-console.log('Hola Mundo!'); //regresa: [00:00:00] Hola Mundo!
-
-//LOG con sistema de filtrado
-console.send('Hola terrible Mundo!'); //regresa: [00:00:00] Hola Mundo!
-```
-
-## [NUEVO] WebSockets
-Emite la consola por medio de un websocket (unicamente soporta Socket.io)
-Solo configura socket.io y agrega el servidor con el parametro `console.createSocket()`
-```js
 const SocketIO = require('socket.io');
-const io = SocketIO();
-const settings = {
-    _dirname: _dirname
-    filter: [
-        "UNUSED",
-        "INFO"
-    ]
-}
 
-const SrConsole = require('sr-console');
-const console = new SrConsole.load(settings);
+const console = new SrConsole.load();
+const io = SocketIO();
 
 console.createSocket(io);
 ```
 
-## ADVERTENCIA
-Este proyecto aun se esta desarrollando
+Para escuchar nuevas entradas en la consola solo debemos usar la siguiente linea de codigo.
+```js
+    socket.on('console', data => {
+        // HAZ ALGO
+    });
+```
+
+## EN DESARROLLO
+Aun estoy trabajando en la libreria. por lo que estare haciendo cambios bastante bruscos en el codigo y en como se redacta con cada cambio realizado hasta que finalice la version 1.0.0. En caso de que la libreria presente algun problema puedes visitar el [Repositorio](https://github.com/Zixasis/sr-console#readme)
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const colors = require('./colors.js');
 const fs = require('fs');
 
 class timestamp {
-    constructor () {
+    constructor() {
         let _date = new Date();
 
         this.hours = _date.getHours();
@@ -20,103 +20,171 @@ class timestamp {
     }
 }
 
-class load {
+class srconsole {
     constructor(settings) {
-        if (settings) {
-            if (settings.dirname) this.filelog = fs.createWriteStream(`${settings.dirname}/log.txt`, { flags: 'a'});
-            try { this.filter = settings.filter; } catch (e) { this.filter = undefined; }
-            // try { this.socket = settings.io;} catch (e) { this.socket = undefined; }
+        this.__params = {
+            socket: undefined,
+            filter: undefined,
+            dirname: undefined,
+            filestream: undefined
         }
 
+        if (typeof settings === "object") {
+            if (settings.dirname) {
+                this.__params.filestream = fs.createWriteStream(`${settings.dirname}/log.txt`, { encoding: "utf-8" });
+                this.__params.dirname = settings.dirname;
+            }
+            if (settings.filter) {
+                this.__params.filter = settings.filter;
+            }
+        }
+        
 
-        this.socket = undefined;
-        this.memory = console.memory;
-        this.createSocket = (io) => {
-            if (!io) return false; 
-            this.socket = io;
-            return true;
-        }
-        this.send = (...argumentus) => {
-            if (!settings && !this.filter) return this.fatalError('NO PUEDES EJECUTAR .send() SIN ESTABLECER PARAMETROS A FILTRAR')
-            let textfiltered = consoleFilter(settings.filter, argumentus.join(' '));
-            console.log(`${colors["blue"]}[${new timestamp().timestring()}]`, textfiltered, colors["red"]);
-            if (this.filelog) this.filelog.write(`b[${new timestamp().timestring()}] ${textfiltered}\n`);
-            if (this.socket) socketEmisor(this.socket, `b[${new timestamp().timestring()}] ${textfiltered}\n`);
-            
-            return this;
-        }
-        this.log = (...argumentus) => {
-            console.log(`${colors["blue"]}[${new timestamp().timestring()}]`, ...argumentus, colors["red"]);
-            if (this.filelog) this.filelog.write(`b[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
-            if (this.socket) socketEmisor(this.socket, `b[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
-            return this;
-        }
-        this.warn = (...argumentus) => {
-            console.warn(`${colors["yellow"]}[${new timestamp().timestring()}]`, ...argumentus, colors["red"]);
-            if (this.filelog) this.filelog.write(`y[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
-            if (this.socket) socketEmisor(this.socket, `y[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
-            return this;
-        }    
-        this.err = (...argumentus) => {
-            console.error(`${colors["red"]}[${new timestamp().timestring()}]`, ...argumentus, colors["red"]);
-            if (this.filelog) this.filelog.write(`r[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
-            if (this.socket) socketEmisor(this.socket, `r[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
-            return this;
-        }
-        this.error = this.err;
-        this.fatalError = (...argumentus) => {
-            console.error(`${colors["yellow"] + colors["bg-red"]}[${new timestamp().timestring()}]`, ...argumentus, colors["reset"] + colors["red"]);
-            if (this.filelog) this.filelog.write(`y bgr[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
-            if (this.socket) socketEmisor(this.socket, `y bgr[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
-            process.exit(1);
-        }
-        this.success = (...argumentus) => {
-            console.log(`${colors["green"]}[${new timestamp().timestring()}]`, ...argumentus, colors["reset"]);
-            if (this.filelog) this.filelog.write(`g[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
-            if (this.socket) socketEmisor(this.socket,`g[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
-            return this;
-        }
-
+        //UNFINISHED FUNCTIONS
+        this.memory = null;
         this.assert = console.assert;
         this.clear = console.clear;
         this.count = console.count;
         this.countReset = console.countReset;
-        this.debug = console.debug;
-        this.dir = console.dir;
-        this.dirxml = console.dirxml;
-        this.group = console.group;
-        this.groupCollapsed = console.groupCollapsed;
-        this.groupEnd = console.groupEnd;
         this.profile = console.profile;
         this.profileEnd = console.profileEnd;
-        this.table = console.table;
         this.time = console.time;
         this.timeEnd = console.timeEnd;
         this.timeLog = console.timeLog;
         this.timeStamp = console.timeStamp;
-        this.trace = console.trace;
+
+        //Directly define
+        this.table = console.table;
+    }
+    
+    /**
+     * @param {WebSocket} io
+     * @description Solo funciona con la libreria de Socket.io
+     */
+    createSocket(io) {
+        if (!io) return { status: false, err: new Error('El parametro "io" no esta definido') };
+        this.__params.socket = io;
+        return { status: true };
+    }
+
+    loadConsole() {
+        try { return fs.readFileSync(`${this.__params.dirname}/log.txt`, { encoding: "utf-8" }); } catch (e) { return new Error(`Ocurrio un error al cargar la consola`); }
+    }
+
+    clearConsole() {
+        try { fs.writeFileSync(`${settingsthis.__params.dirname}`, ""); } catch (e) { /* Nothing */ }
+    }
+
+
+
+    //PARAMS
+    send(...argumentus) {
+        if (!this.__params.filter) return this.log(...argumentus);
+        let textfiltered = consoleFilter(this.__params.filter, argumentus.join(' '));
+        console.log(`${colors["blue"]}[${new timestamp().timestring()}]`, textfiltered, colors["red"]);
+        if (this.__params.filestream) this.__params.filestream.write(`%bl%[${new timestamp().timestring()}] ${textfiltered}\n`);
+        if (this.__params.socket) socketEmisor(this.__params.socket, `%bl%[${new timestamp().timestring()}] ${textfiltered}\n`);
+        return this;
+    }
+    log(...argumentus) {
+        console.log(`${colors["blue"]}[${new timestamp().timestring()}]`, ...argumentus, colors["red"]);
+        if (this.__params.filestream) this.__params.filestream.write(`%bl%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        if (this.__params.socket) socketEmisor(this.__params.socket, `%bl%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        return this;
+    }
+    debug(...argumentus) {
+        console.log(`${colors["cyan"]}[${new timestamp().timestring()}]`, ...argumentus, colors["red"]);
+        if (this.__params.filestream) this.__params.filestream.write(`%cy%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        if (this.__params.socket) socketEmisor(this.__params.socket, `%cy%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        return this;
+    }
+    warn(...argumentus) {
+        console.warn(`${colors["yellow"]}[${new timestamp().timestring()}]`, ...argumentus, colors["red"]);
+        if (this.__params.filestream) this.__params.filestream.write(`%yl%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        if (this.__params.socket) socketEmisor(this.__params.socket, `%yl%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        return this;
+    }
+    err(...argumentus) {
+        console.error(`${colors["red"]}[${new timestamp().timestring()}]`, ...argumentus, colors["red"]);
+        if (this.__params.filestream) this.__params.filestream.write(`%rd%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        if (this.__params.socket) socketEmisor(this.__params.socket, `%rd%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        return this;
+    }
+    error(...argumentus) {
+        console.error(`${colors["red"]}[${new timestamp().timestring()}]`, ...argumentus, colors["red"]);
+        if (this.__params.filestream) this.__params.filestream.write(`%rd%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        if (this.__params.socket) socketEmisor(this.__params.socket, `%rd%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        return this;
+    }
+    fatalError(...argumentus) {
+        console.error(`${colors["yellow"] + colors["bg-red"]}[${new timestamp().timestring()}]`, ...argumentus, colors["reset"] + colors["red"]);
+        if (this.__params.filestream) this.__params.filestream.write(`%y% %bgrd%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        if (this.__params.socket) socketEmisor(this.__params.socket, `%y% %bgrd%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        process.exit(1);
+    }
+    success(...argumentus) {
+        console.log(`${colors["green"]}[${new timestamp().timestring()}]`, ...argumentus, colors["reset"]);
+        if (this.__params.filestream) this.__params.filestream.write(`%gn%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        if (this.__params.socket) socketEmisor(this.__params.socket, `%gn%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        return this;
+    }
+    dir(argumentu, options) {
+        console.dir(argumentu, options)
+    }
+    group(...argumentus) {
+        console.group(`${colors["magenta"]}[${new timestamp().timestring()}]`, ...argumentus, colors["reset"]);
+        if (this.__params.filestream) this.__params.filestream.write(`%mg%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        if (this.__params.socket) socketEmisor(this.__params.socket, `%mg%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        return this;
+    }
+    groupEnd() {
+        console.groupEnd();
+        return this;
+    }
+    trace(...argumentus) {
+        console.trace(`${colors["yellow"] + colors["bg-red"]}[${new timestamp().timestring()}]`, ...argumentus, colors["reset"] + colors["red"]);
+        if (this.__params.filestream) this.__params.filestream.write(`%y% %bgrd%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        if (this.__params.socket) socketEmisor(this.__params.socket, `%y% %bgrd%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+    }
+
+
+    //DEPRECATED FUNCTIONS [SE ELIMINARAN EN LA VERSION FINAL]
+    /**
+     * @deprecated
+     */
+    groupCollapsed() {
+        console.group(`${colors["magenta"]}[${new timestamp().timestring()}]`, ...argumentus, colors["reset"]);
+        if (this.__params.filestream) this.__params.filestream.write(`%gn%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        if (this.__params.socket) socketEmisor(this.__params.socket, `%gn%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        return this;
+    }
+    /**
+     * @deprecated
+     * @param  {...any} argumentus 
+     */
+    dirxml(...argumentus) {
+        console.log(`${colors["blue"]}[${new timestamp().timestring()}]`, ...argumentus, colors["red"]);
+        if (this.__params.filestream) this.__params.filestream.write(`%bl%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        if (this.__params.socket) socketEmisor(this.__params.socket, `%bl%[${new timestamp().timestring()}] ${argumentus.join(' ')}\n`);
+        return this;
     }
 }
 
 function consoleFilter(filter, value) {
     let result = value;
-    for(let i = 0; i <= Object.keys(filter).length; i++) {
-      result = result.replace(filter[i], "");
-  
-      if (i == Object.keys(filter).length) return result;
+    for (let i = 0; i <= Object.keys(filter).length; i++) {
+        result = result.replace(filter[i], "");
+        if (i == Object.keys(filter).length) return result;
     }
-  }
+}
 
 function socketEmisor(io, value) {
     try {
-        io.sockets.emit("console" , value);
+        io.sockets.emit("console", value);
         return { status: true, err: null }
     } catch (e) {
         return { status: false, err: e };
     }
 }
 
-module.exports = {
-    load: load,
-    timestamp: timestamp
-}
+module.exports = srconsole;
