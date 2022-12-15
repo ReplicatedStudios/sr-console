4b42d1cf583d263e4493645a98cb8cdf1efbedcc
SrRapero720 <SrRapero720@hotmail.com>
2022-12-15 02:35:09 -0600

    test actualizados

--- testing/basics.test.js
+++ testing/basics.test.js
@@ -1,4 +1,4 @@
-import "../modules/build/index.js";
+require("../common/build/index");
 
 console.time();
 console.warn("Iniciando demostracion de SR-CONSOLE");
@@ -7,22 +7,29 @@ console.log("Cargando servidor ficticio");
 console.log("Revisando informacion");
 console.success("Toda la informacion fue validada");
 console.group("Cargando registros")
-console.debug("Se han detectado 428 registros");
+console.debug("Se han detectado", 393, "registros");
 console.debug("Importando registros");
 console.debug("Indexandolos al nucleo del servidor");
+console.group("Revision de registros")
 console.err("Se detectaron 43 registros no validos o dañados");
-console.unGroup("Se han cargado con exito +350 registros");
+console.info("Se intento reparar 43 registros");
+console.warn("Existen", 4, "registros que se encuentran en un estado critico e irreparable")
+console.unGroup("Se han cargado con exito", 350, "registros");
 
 console.warn("Iniciando servidor");
 console.log("Iniciando sesion");
 console.send("Enviando Token NIGGA-XXXXXX-XXXXX");
 console.success("Se ha iniciado session exitosamente")
 
-console.info("Estan pendientes los 43 registros sin cargar");
-console.info("el servidor se encuentra usando 43gb de ram")
+console.info("Estan pendientes los",43, "registros sin cargar");
+console.info("el servidor se encuentra usando 60gb de ram")
 console.info("no se pudo procesar el modelo del procesador, se utilizo el modo seguro");
 setTimeout(() => {
     console.timeEnd();
-    console.clear()
+    console.fatalBack(new Error("FATAL ERROR OCURRED ON SYSTEM REGISTRY"));
+    console.log("recuperado");
+    console.warn("oh no");
+    console.err("no me siento bien");
+    throw new Error("Finalizado");
 }, 2000);
 
--- testing/complex.test.js
+++ testing/complex.test.js
@@ -1,19 +1,9 @@
-import SrPrint from "../modules/build/index.js";
+import "../modules/build/index.js";
 
-console.count("count");
-console.count("count");
-console.count("count");
-console.count("count");
-console.count("count");
-console.count("count");
-console.count("count");
-console.count("count");
-console.countReset("count")
-console.count("count");
-console.count("count");
-console.count("count");
+console.log(1+1+1+1+(1*0)+1);
+console.log(1+1+1+1+1*0+1);
 
-const printer = new SrPrint("COMPLEX");
+const printer = new LOG.SrPrint("COMPLEX");
 printer.send("W", "NUEVO PRINTER CREADO");
 printer.send("D", "Debugeando con printer");
 printer.send("S", "printer probado exitosamente", "iniciando proceso de creado de", 4, "subprinters");
